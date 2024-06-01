164b5bcc4feeec5f72f83651e0eb6a2a19a8b896
Vibhav <vibhavsachan743@gmail.com>
2024-06-01 23:21:52 +0530

    weather

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ce5e288796d46df62b507de99950b8dd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
